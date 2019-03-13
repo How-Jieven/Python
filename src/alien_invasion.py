d98b695fad2ec70a1b8ee1510fc5d5b24321cdeb
How-Jieven <523772121@qq.com>
2019-03-13 23:51:14 +0800

    优化前台调用界面

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship 
+import game_functoins as gf
 def run_game():
     print('execute run_game')
     #初始化游戏并创建一个游戏屏幕对象
@@ -15,17 +16,19 @@ def run_game():
     #设置背景颜色
     #bg_color=(230,230,230)
     #创建一艘飞船
-    ship=Ship(screen)
+    ship=Ship(ai_settings,screen)
     while True:
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                sys.exit()
-                
+       # for event in pygame.event.get():
+          #  if event.type==pygame.QUIT:
+           #     sys.exit()
+        gf.check_events(ship)
+        ship.update(ai_settings)
         #每次循环的时候都重绘屏幕
         #screen.fill(bg_color)
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
+       # screen.fill(ai_settings.bg_color)
+        #ship.blitme()
         #每次循环时都重绘屏幕（让最近绘制的屏幕可见）
-        pygame.display.flip()
+        #pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 run_game()
