b46f963ebbdb04c09e67c23397ae73400bde69af
How-Jieven <523772121@qq.com>
2019-03-30 22:57:07 +0800

    似乎出了点问题

--- src/game_functoins.py
+++ src/game_functoins.py
@@ -10,6 +10,9 @@ import pygame
 from bullet import Bullet
 
 from ship import Ship
+from alien import Alien
+from pygame.examples import aliens
+from pygame.examples.headless_no_windows_needed import screen
 
 
 def check_events(ai_settings, screen, ship, bullets):
@@ -40,7 +43,7 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """更新屏幕上的图像，并切换到新的屏幕"""
     # 每次循环的时候都切换屏幕
     screen.fill(ai_settings.bg_color)
@@ -49,7 +52,9 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
 
     ship.blitme()
-
+   # 对编组调用draws的时候，Python会自动绘制编组的每一个元素，绘制的位置有每一个元素的rect决定
+    aliens.draw(screen)
+    # alien.blitme()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
@@ -95,3 +100,38 @@ def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullet_allowed:
         new_Bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_Bullet)
+
+
+def creat_fleet(ai_settings, screen, aliens):
+    """创建外星人群"""
+    # 创建一个外星人，并计算一行可以容纳多少个外星人
+    # 外星人间距是外星人的宽度
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    #number_aliens_x = get_number_alines_x(ai_settings, alien.rect.width)
+    # 创建第一个外星人
+    for alien_number in range(number_aliens_x):
+        # 创建一个外星人并将其加入当前行
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
+        #creat_alien(ai_settings, screen, aliens, alien_number)
+
+
+def get_number_alines_x(ai_settings, alien_width):
+    """计算每行可以容纳多少个外星人（屏幕宽度-2倍的外星人宽度）除以外星人宽度"""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+
+def creat_alien(ai_settings, screen, aliens, alien_number):
+    """创建一个外星人并将其放在当前行"""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
