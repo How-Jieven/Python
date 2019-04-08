3aa4201e21ad57442b6dd3810c4ef90a1f9e28d7
How-Jieven <523772121@qq.com>
2019-04-08 23:42:30 +0800

    提高等级

--- src/settings.py
+++ src/settings.py
@@ -22,13 +22,32 @@ class Settings():
 
         # 子弹的设置
         self.bullet_speed_factor = 3
-        self.bullet_width = 300
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullet_allowed = 3
 
         # 外星人的设置
         self.aline_speed_factor = 1
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 10
         # fleet_direction为1表示向右，为-1表示向左
         self.fleet_direction = 1
+
+        # 以什么样的速度加快游戏节奏
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed_factor = 15000
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction为1表示向右；为-1表示向左
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """提高速度设置"""
+        self.sheip_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
